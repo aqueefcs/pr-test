aaaae28af63d6f328e583b85294ec8b6708e6e69
aqueefcs <aqueefalimansari007@gmail.com>
2024-04-01 01:22:54 +0530

    pr test

--- Code-Generation/index.js
+++ Code-Generation/index.js
@@ -1,13 +1,1 @@
-const fs = require("fs");
-
-const number = JSON.parse(fs.readFileSync("./number.json"));
-console.log(number);
-
-const randomNumber = Math.random() * 100;
-console.log(randomNumber);
-
-const updated = number.number.push(randomNumber);
-console.log(number.number);
-
-fs.writeFileSync("./number.json", JSON.stringify(number), "utf8");
 console.log("Nodejs script Executed");
