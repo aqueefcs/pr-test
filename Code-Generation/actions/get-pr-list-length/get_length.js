66f28e32087671e157c6ab2ad68817efa16d3f7e
aqueefcs <aqueefalimansari007@gmail.com>
2024-04-03 01:27:17 +0530

    pr test

--- Code-Generation/actions/get-pr-list-length/get_length.js
+++ Code-Generation/actions/get-pr-list-length/get_length.js
@@ -3,7 +3,7 @@ require("dotenv/config");
 const pr_array = JSON.parse(process.env.PR_ARRAY);
 
 if (pr_array.length == 0) {
-  console.log(pr.length);
+  console.log(pr_array.length);
 } else {
   console.log(true);
 }
