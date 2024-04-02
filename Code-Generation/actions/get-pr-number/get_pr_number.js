2a06e04a41de30eeb96bfeb2b9701204ef91cdbb
aqueefcs <aqueefalimansari007@gmail.com>
2024-04-03 01:07:13 +0530

    pr test

--- Code-Generation/actions/get-pr-number/get_pr_number.js
+++ Code-Generation/actions/get-pr-number/get_pr_number.js
@@ -4,14 +4,10 @@ const core = require("@actions/core");
 const branch = process.env.BRANCH;
 const pr_array = JSON.parse(process.env.PR_ARRAY);
 
-console.log(`Branch: ${branch}`);
-// console.log(`PR array: ${pr_array}`);
-
 for (item of pr_array) {
-  console.log(item.head.ref);
   if (item.head.ref == `${branch}`) {
     console.log(item.head.ref);
   } else {
-    console.log("Pr Not Present");
+    console.log(false);
   }
 }
