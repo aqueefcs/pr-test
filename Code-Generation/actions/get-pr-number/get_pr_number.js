c4cb9e24ec6a4b8c636843141e121a24a7713889
aqueefcs <aqueefalimansari007@gmail.com>
2024-04-03 00:15:11 +0530

    pr test

---
+++ Code-Generation/actions/get-pr-number/get_pr_number.js
@@ -0,0 +1,15 @@
+require("dotenv/config");
+const core = require("@actions/core");
+
+const branch = process.env.branch_name;
+const pr_array = JSON.parse(process.env.pr_array);
+
+console.log(`Branch: ${branch}`);
+// console.log(`PR array: ${pr_array}`);
+
+for (item of pr_array) {
+  if (item.head.ref === branch) {
+    console.log(item.head.ref);
+    core.setOutput("PR_NUMBER", item.number);
+  }
+}
