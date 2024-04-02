3cbc71b7ecd7b6626ac2b85bb40b8e4a063f2335
aqueefcs <aqueefalimansari007@gmail.com>
2024-04-02 19:54:44 +0530

    pr test

--- Code-Generation/get_pr_number.js
+++ Code-Generation/get_pr_number.js
@@ -1,9 +1,9 @@
 require("dotenv/config");
 
 const branch_name = process.env.branch_name;
-const pr_array = process.env.pr_array;
+const pr_array = JSON.parse(process.env.pr_array);
 
-// console.log(`Branch: ${branch_name}`);
+console.log(`Branch: ${branch_name}`);
 // console.log(`PR array: ${pr_array}`);
 
 for (item of pr_array) {
