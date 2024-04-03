66b2b410bc8e1f1d41e65702e0da1ee0d14896a9
aqueefcs <aqueefalimansari007@gmail.com>
2024-04-03 15:26:20 +0530

    pr test

--- Code-Generation/actions/get-after-pr-number/get_pr_number.js
+++ Code-Generation/actions/get-after-pr-number/get_pr_number.js
@@ -6,6 +6,7 @@ const pr_array = JSON.parse(process.env.PR_ARRAY);
 for (item of pr_array) {
   if (item.head.ref == `${branch}`) {
     console.log(item.number);
+    break;
   } else {
     console.log("false");
   }
--- Code-Generation/after_pr_number.js
+++ Code-Generation/after_pr_number.js
@@ -6,6 +6,7 @@ const pr_array = JSON.parse(process.env.after_pr_array);
 for (item of pr_array) {
   if (item.head.ref == `${branch}`) {
     console.log(item.number);
+    break;
   } else {
     console.log("false");
   }
