cd49bea848fe83e17657cbbe2259fb61e1002cc3
aqueefcs <aqueefalimansari007@gmail.com>
2024-04-09 12:46:01 +0530

    pr test

--- Code-Generation/actions/get-pr-list-length/get_length.js
+++ Code-Generation/actions/get-pr-list-length/get_length.js
@@ -1,9 +1,16 @@
 require("dotenv/config");
+const axios = require("axios");
 
-const pr_array = JSON.parse(process.env.PR_ARRAY);
-
-if (pr_array.length == 0) {
-  console.log(`${pr_array.length}`);
-} else {
-  console.log("true");
-}
+axios
+  .get("https://api.github.com/repos/aqueefcs/pr-test/pulls")
+  .then((response) => {
+    const pr_list = response.data;
+    if (pr_list.length == 0) {
+      console.log(`${pr_list.length}`);
+    } else {
+      console.log("true");
+    }
+  })
+  .catch((err) => {
+    throw Error(`Error in Fetching pull request list: ${err}`);
+  });
--- Code-Generation/actions/get-pr-number/get_pr_number.js
+++ Code-Generation/actions/get-pr-number/get_pr_number.js
@@ -1,13 +1,21 @@
 require("dotenv/config");
+const axios = require("axios");
 
-const branch = process.env.BRANCH;
-const pr_array = JSON.parse(process.env.PR_ARRAY);
-
-for (item of pr_array) {
-  if (item.head.ref == `${branch}`) {
-    console.log(item.number);
-    break;
-  } else {
-    console.log("false");
-  }
-}
+axios
+  .get("https://api.github.com/repos/aqueefcs/pr-test/pulls")
+  .then((response) => {
+    const pr_list = response.data;
+    // const branch = "RITM0130572";
+    const branch = process.env.BRANCH;
+    for (item of pr_list) {
+      if (item.head.ref == `${branch}`) {
+        console.log(item.number);
+        break;
+      } else {
+        console.log("false");
+      }
+    }
+  })
+  .catch((err) => {
+    throw Error(`Error in Fetching pull request list: ${err}`);
+  });
